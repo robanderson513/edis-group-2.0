7671ca5c48c25e8be8c691c81ad3a667f37744bf
Rob Anderson 🏂 <randerson@assurecare.com>
2022-03-26 16:20:00 -0400

    Added transitions with css

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { interval } from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -6,6 +7,11 @@ import { tap } from 'rxjs/operators';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('slideAnimation', [
+      transition(':enter', [style({ transform: 'translateY(100%)', opacity: 0 }), animate('500ms', style({ transform: 'translateY(0)', opacity: 1 }))])
+    ])
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
