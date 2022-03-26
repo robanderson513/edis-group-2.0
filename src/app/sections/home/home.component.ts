a8c026c5f05dd95415e7329c555b5bd61aabaa3f
Rob Anderson 🏂 <randerson@assurecare.com>
2022-03-26 14:36:36 -0400

    Updated image zoom in

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { interval } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,22 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
+  public activeImage: number = 0;
 
-  constructor() { }
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+    interval(10000)
+      .pipe(tap(() => this.changeDisplay()))
+      .subscribe();
   }
 
+  private changeDisplay(): void {
+    this.activeImage = this.activeImage === 0 ? 1 : this.activeImage === 1 ? 2 : 0;
+    this.changeDetectorRef.detectChanges();
+
+    //   ('straight-shooting. recruiting.');
+    // ('personal. recruiting.');
+    // ('focused. recruiting.');
+  }
 }
